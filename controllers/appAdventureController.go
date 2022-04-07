df9005ec029887ddc96c3bc6d2ca71c4e420926a
Alex Turner <alex.turner@safetyculture.io>
2022-04-08 00:05:56 +1000

    experimenting with room structure

--- controllers/appAdventureController.go
+++ controllers/appAdventureController.go
@@ -1,22 +1,38 @@
 package controllers
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 
 	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
+
 	"github.com/slack-go/slack/socketmode"
 )
 
 // We create a sctucture to let us use dependency injection
 type AppAdventureController struct {
 	EventHandler *socketmode.SocketmodeHandler
+	Rooms        []Rooms
 }
 
 func NewAppAdventureController(eventhandler *socketmode.SocketmodeHandler) AppAdventureController {
+	file, err := ioutil.ReadFile("data/rooms.json")
+	if err != nil {
+		log.Printf("ERROR ReadFile: %v", err)
+	}
+
 	c := AppAdventureController{
 		EventHandler: eventhandler,
 	}
+
+	err = json.Unmarshal([]byte(file), &c.Rooms)
+	if err != nil {
+		log.Printf("ERROR unmarshal: %v", err)
+	}
+
+	log.Printf("ERROR unmarshal: %v", c.Rooms)
 
 	c.EventHandler.HandleEventsAPI(
 		slackevents.Message,
@@ -26,8 +42,23 @@ func NewAppAdventureController(eventhandler *socketmode.SocketmodeHandler) AppAd
 	return c
 }
 
+type Rooms struct {
+	ID          string // guid?
+	Description string
+	Exits       []Exits
+}
+
+// Exits:  n,e,s,w [10        ,11,0,34] ? zero as null/nothing?
+// Exits2: array/slice of type exits, allowing for 0 to n exits from a room.
+
+type Exits struct {
+	Key         string // eg nrth/n
+	Description string
+	Destination string // destination key/id
+}
+
 func (c *AppAdventureController) processMessage(evt *socketmode.Event, clt *socketmode.Client) {
-	// we need to cast our socketmode.Event into slackevents.AppHomeOpenedEvent
+	// we need to cast our socketmode.Event into slackevents.MessageEvent
 	evt_api, ok := evt.Data.(slackevents.EventsAPIEvent)
 
 	clt.Ack(*evt.Request) // Acknowlege message otherwise it will retry
